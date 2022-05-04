58afea235bf7a56f764bdd68282805db9fb8332b
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-05-04 18:31:24 +0200

    form OK

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,23 @@
 new Date().getFullYear()  // returns the current year
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+function formInputNotEmpty() {
+    var emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    var $$mail = document.getElementById("email")
+    if(
+        document.getElementById("name").value=== ""
+        || document.getElementById("email").value===""
+        || !$$mail.value.match(emailReg)
+        || document.getElementById("message").value===""
+        || !document.getElementById("privacy").checked){
+
+           document.getElementById('submit-button-form').disabled = true;
+           document.getElementById('tooltiptext').style.display  = "inline-block";
+
+        } else {
+            document.getElementById('submit-button-form').disabled = false;
+            document.getElementById('tooltiptext').style.display  = "none";
+       }
+   }
+
